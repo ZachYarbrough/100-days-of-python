cf588ad47ee836151eaf850bc6c01260aabdb9d0
Zach Yarbrough <zachyarbro@gmail.com>
2022-05-29 14:14:09 -0500

    (005): Finished Module Work

---
+++ day-005/main.py
@@ -0,0 +1,14 @@
+# Loop through Python Lists
+
+list_of_items = ['1', '2', '3']
+
+for item in list_of_items:
+    print(item)
+
+print('================================')
+# For Loop with a range, kinda like index
+# for (let i = 1; i < 11; i += 3)
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
