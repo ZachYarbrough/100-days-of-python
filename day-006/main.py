5eb5eb47da4be89b51b44de9c1244983b540ef5b
Zach Yarbrough <zachyarbro@gmail.com>
2022-05-30 10:58:56 -0500

    (006): Finished Module Work

---
+++ day-006/main.py
@@ -0,0 +1,12 @@
+# Functions
+
+def my_function():
+    print("This is inside a function")
+
+# While Loop
+
+num_of_prints = 6
+
+while num_of_prints > 0:
+    my_function()
+    num_of_prints -= 1
