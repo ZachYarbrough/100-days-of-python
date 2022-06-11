b93ed93f6c140a9c98c45b23083e7eb5d16234b8
Zach Yarbrough <zachyarbro@gmail.com>
2022-06-11 15:38:49 -0500

    (018): Finished Module Work

---
+++ day-018/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.shape('turtle')
+timmy.color('green')
+
+timmy.forward(10)
+timmy.penup()
+timmy.forward(10)
+timmy.pendown()
+timmy.forward(10)
+timmy.penup()
+timmy.forward(10)
+timmy.forward(10)
+timmy.penup()
+timmy.forward(10)
+timmy.pendown()
+timmy.forward(10)
+timmy.penup()
+timmy.forward(10)
+
+
+screen = Screen()
+screen.exitonclick()
