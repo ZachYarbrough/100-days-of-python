3c948e0298ef16eae39f1d4dee7d6d6983793a49
Zach Yarbrough <zachyarbro@gmail.com>
2022-06-24 09:30:09 -0500

    (020): Finished Snake Class

---
+++ day-020/snake.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+move_dist = 20
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+        
+    def create_snake(self):
+        for x, y in starting_positions:
+            segment = Turtle("square")
+            segment.color("#64d86b") 
+            segment.penup()
+            segment.goto(x, y)
+            self.segments.append(segment)
+
+    def move(self):
+        for i in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[i - 1].xcor()
+            new_y = self.segments[i - 1].ycor()
+            self.segments[i].goto(new_x, new_y)
+        self.head.forward(move_dist)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+    
